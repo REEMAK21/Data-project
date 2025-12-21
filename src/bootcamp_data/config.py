b86cf956afe6fb61814a52995b3b20c8ce318d60
REEMAK21 <alkhaldyryma657@gmail.com>
2025-12-21 18:34:49 +0300

    Add gitignore

---
+++ src/bootcamp_data/config.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from pathlib import Path 
+
+@dataclass(frozen=True)
+class Paths:
+    root:Path
+    raw: Path
+    cache: Path
+    processed: Path
+    external: Path
+def make_paths(root: Path) ->Paths:
+    data= root/ "data"
+    return Paths(
+        root=root,
+        raw=data/"cache",
+        processed=data/"processed",
+        external=data/"external",
+
+    )
